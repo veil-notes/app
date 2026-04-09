c822f040b413c868415cd726b5b5bb0de37e08e4
Danilo Lutz <danilolutz@proton.me>
2026-04-09 17:28:50 -0300

    build(android): keep PathUtils in release builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -52,6 +52,10 @@ android {
     buildTypes {
         release {
            signingConfig = signingConfigs.getByName("release")
+           proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
         }
     }
 }
